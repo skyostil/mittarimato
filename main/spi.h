7ab03cf9bbf396219d36dedbaace62792b00a9d9
Sami Kyöstilä <sami.kyostila@unrealvoodoo.org>
2020-06-28 11:41:07 +0100

    Document pin connections

--- main/spi.h
+++ main/spi.h
@@ -3,10 +3,4 @@
 #include <driver/gpio.h>
 #include <driver/spi.h>
 
-//constexpr static gpio_num_t kSPIPinSCLK = GPIO_NUM_14;  // D5 <--> SCL
-//constexpr static gpio_num_t kSPIPinMOSI = GPIO_NUM_13;  // D7 <--> SDA/DIN
-//constexpr static gpio_num_t kSPIPinRST = GPIO_NUM_12;   // D6 <--> RES
-//constexpr static gpio_num_t kSPIPinDC = GPIO_NUM_15;    // D8 <--> D/C
-//constexpr static gpio_num_t kSPIPinCS = GPIO_NUM_16;    // D0 <--> CS
-
 void SetupSPI();
